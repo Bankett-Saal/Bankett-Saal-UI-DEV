22ec055eef15673714e67b491269af643080f0d0
ShivaBanqettsaalz <112945079+ShivaBanqettsaalz@users.noreply.github.com>
2022-09-22 16:52:50 +0530

    Update searchbar.service.ts

--- src/app/UserModule/services/searchbar.service.ts
+++ src/app/UserModule/services/searchbar.service.ts
@@ -14,12 +14,12 @@ export class SearchbarService {
   constructor(private http: HttpClient) { }
   getAllFunctionHalls():Observable<any>
   {
-    return this.http.get('http://bankett-saalz-service-ALB-1085803072.ap-south-1.elb.amazonaws.com:8057/api/functionhalls');
+    return this.http.get('http://4.195.89.112:8057/api/functionhalls');
   }
   
   findFunctionhallByNameAndCity(city):Observable<any>
   {
-    return this.http.get('http://bankett-saalz-service-ALB-1085803072.ap-south-1.elb.amazonaws.com:8057/api/functionhallsBy/?city='+city);
+    return this.http.get('http://4.195.89.112:8057/api/functionhallsBy/?city='+city);
   }
  
 }
