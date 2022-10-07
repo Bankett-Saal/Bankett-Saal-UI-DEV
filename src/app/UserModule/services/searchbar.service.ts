cd7c6ef09932f7040e411c6adc7f7f67e73514d7
ShivaBanqettsaalz <112945079+ShivaBanqettsaalz@users.noreply.github.com>
2022-10-07 11:31:37 +0530

    Update searchbar.service.ts

--- src/app/UserModule/services/searchbar.service.ts
+++ src/app/UserModule/services/searchbar.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable } from 'rxjs';
 // import { EnvironmentUrlService } from './../../../environments/EnvironmentUrlService';
-import { environment } from './../../../environments/environment';
+// import { environment } from './../../../environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -13,10 +13,10 @@ const httpOptions = {
 export class SearchbarService {
 
   // constructor(private http: HttpClient, private envUrl: ./../../../environments/environment) { }
-  constructor(private envUrl: ./../../../environments/environment) { }
+    constructor(private http: HttpClient) { }
   getAllFunctionHalls():Observable<any>
   {
-    return this.http.get(this.createCompleteRoute(route, this.envUrl.urlAddress));
+    return this.http.get('http://13.233.232.133:8057/api/functionhalls');
   }
   
   findFunctionhallByNameAndCity(city):Observable<any>
