1f379278ebff6a739a64cab68d822366cd84854b
ShivaBanqettsaalz <112945079+ShivaBanqettsaalz@users.noreply.github.com>
2022-10-07 12:56:54 +0530

    Update searchbar.service.ts

--- src/app/UserModule/services/searchbar.service.ts
+++ src/app/UserModule/services/searchbar.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable } from 'rxjs';
-import { EnvironmentUrlService } from './../../environment-url.service';
+import { EnvironmentUrlService } from './src/environments/environment-url.service';
 // import { environment } from './../../environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
