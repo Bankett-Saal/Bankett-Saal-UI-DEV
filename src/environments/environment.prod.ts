c2c1e0dab5b04d8570a435432d10673d851a8434
ShivaBanqettsaalz <112945079+ShivaBanqettsaalz@users.noreply.github.com>
2022-10-07 11:49:11 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+//export const environment = {
+  //production: true
+//};
+
+
 export const environment = {
-  production: true
+  production: true,
+  // title: 'Production Environment Heading',
+  apiURL: 'https://apiexample.com'
 };
